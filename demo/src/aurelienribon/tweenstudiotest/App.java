1c2ea0060b4a199b6e3f1664622a13971a7458dc
AurelienRibon <AurelienRibon@localhost>
2012-01-11 11:09:59 +0100

    * updated comments in the demo

--- demo/src/aurelienribon/tweenstudiotest/App.java
+++ demo/src/aurelienribon/tweenstudiotest/App.java
@@ -26,40 +26,60 @@ public class App implements ApplicationListener {
 		Tween.enablePooling(true);
 		Tween.registerAccessor(Sprite.class, new SpriteTweenAccessor());
 
-		// Tween Studio initialization: this single call ("enableEdition()")
-		// lets you toggle between edition and play modes!
+		// ---------------------------------------------------------------------
+		// 0. Tween Studio initialization: this single call ("enableEdition()")
+		//    lets you toggle between edition and play modes!
 		int res = JOptionPane.showConfirmDialog(null, "Do you want to enable the edition of the animations ?", "Tween Studio", JOptionPane.YES_NO_OPTION);
 		if (res == 0) TweenStudio.enableEdition();
+		// ---------------------------------------------------------------------
 	}
 
 	@Override
 	public void create() {
-		TweenStudio.loadAnimation(Gdx.files.internal("data/anim1.timeline").file(), ANIMATION_1);
-		TweenStudio.loadAnimation(Gdx.files.internal("data/anim2.timeline").file(), ANIMATION_2);
-
+		// Common game creation stuff
 		float ratio = (float)Gdx.graphics.getWidth()/Gdx.graphics.getHeight();
 		camera = new OrthographicCamera(10, 10/ratio);
 		spriteBatch = new SpriteBatch();
 		tweenManager = new TweenManager();
 		createSprites();
 
+		// ---------------------------------------------------------------------
+		// 1. Preloading of the animations. You should always do that in your
+		//    initialization code and not while your game is running, for
+		//    performance reason.
+		TweenStudio.loadAnimation(Gdx.files.internal("data/anim1.timeline").file(), ANIMATION_1);
+		TweenStudio.loadAnimation(Gdx.files.internal("data/anim2.timeline").file(), ANIMATION_2);
+
+		// 2. Registration of the editor we want to use for the next animations.
 		TweenStudio.registerEditor(LibGdxTweenStudioEditorX.class);
+
+		// 2b. The provided LibGdxTweenStudioEditorX editor needs to be
+		//     configurated. Be careful that when edition is disabled, all
+		//     editor instances are null.
 		LibGdxTweenStudioEditorX editor = TweenStudio.getEditor(LibGdxTweenStudioEditorX.class);
 		if (editor != null) editor.setup(camera);
-		
-		TweenStudio.unregisterAllTargets();
+
+		// 3. Registration of the objects that are part of the next animation.
+		//    Their names are required to create timelines from serialized
+		//    content.
 		TweenStudio.registerTarget(sprites[0], "Logo LibGDX");
 		TweenStudio.registerTarget(sprites[1], "Logo Tween Engine");
 		TweenStudio.registerTarget(sprites[2], "Logo Tween");
 		TweenStudio.registerTarget(sprites[3], "Logo Studio");
-		
+
+		// 4. Creation of the first animation timeline. We also add it to our
+		//    TweenManager. In edition mode, it won't be started automatically,
+		//    to let you edit it.
 		TweenStudio.createTimeline(ANIMATION_1).start(tweenManager);
+
+		// That's all! The following lines let you edit a second animation once
+		// you're happy with the first one.
+		// ---------------------------------------------------------------------
 
 		TweenStudio.unregisterAllTargets();
 		TweenStudio.registerTarget(sprites[4], "Wave 1");
 		TweenStudio.registerTarget(sprites[5], "Wave 2");
 		TweenStudio.registerTarget(sprites[6], "Wave 3");
-
 		TweenStudio.createTimeline(ANIMATION_2).start(tweenManager);
 	}
 
@@ -86,8 +106,14 @@ public class App implements ApplicationListener {
 	@Override
 	public void render() {
 		int delta = (int) (Gdx.graphics.getDeltaTime() * 1000);
-		TweenStudio.update(delta);
 		tweenManager.update(delta);
+
+		// ---------------------------------------------------------------------
+		// 5. You need to update the studio periodically (only required with
+		//    some editors, like LibGdxTweenStudioEditorX). This won't do
+		//    anything in play mode.
+		TweenStudio.update(delta);
+		// ---------------------------------------------------------------------
 
 		GL10 gl = Gdx.gl10;
 		gl.glClearColor(1, 1, 1, 1);
@@ -99,7 +125,12 @@ public class App implements ApplicationListener {
 			sprites[i].draw(spriteBatch);
 		spriteBatch.end();
 
+		// ---------------------------------------------------------------------
+		// 6. Some editors (like LibGdxTweenStudioEditorX) render something in
+		//    overlay of your game, so you need to add this line too. Again,
+		//    this call doesn't cost anything in play mode.
 		TweenStudio.render();
+		// ---------------------------------------------------------------------
 	}
 
 	@Override public void resize(int w, int h) {}
