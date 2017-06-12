2a9ca9eace888e8c39951947d93ad36ab5f964ff
David <david.wursteisen+git@gmail.com>
2017-06-12 21:24:58 +0200

    runtime

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -1,9 +1,13 @@
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 import aurelienribon.tweenstudiotest.App;
 
 public class Main {
     public static void main(String[] args) {
+
+        ShaderProgram.pedantic = false;
+
         new LwjglApplication(new App(), "My Super Awesome Game !", 320, 480);
     }
 }
